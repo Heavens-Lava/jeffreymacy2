0d7dd80a2291fb750340d6a45a2422e5c5da1f89
Jeffrey Macy <yosoytujeffe@gmail.com>
2025-05-25 08:21:52 -0700

    new update

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
         <InfiniteMarquee />
         <Portfolio />
         <Experience />
-        <Room />
+        {/* <Room /> */}
         <RoomPage2 />
         <Contact />
 
